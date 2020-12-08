f8fc7659d952dd61b9546330cc4fe33c9fe1e7ed
grzesmich <michal.grzesik@orange.com>
2020-12-08 12:28:15 +0100

    pylint and pydocstyle fixed

--- src/onapsdk/cds/blueprint_model.py
+++ src/onapsdk/cds/blueprint_model.py
@@ -1,4 +1,5 @@
 """CDS Blueprint Models module."""
+
 from typing import Iterator
 
 from .blueprint import Blueprint
@@ -6,24 +7,39 @@ from .cds_element import CdsElement
 
 
 class BlueprintModel(CdsElement):  # pylint: disable=too-many-instance-attributes
-    """Blueprint Model class
+    """Blueprint Model class.
 
     Represents blueprint models in CDS
     """
 
-    def __init__(self,
-                 blueprint_model_id,
-                 artifact_uuid=None,
-                 artifact_type=None,
-                 artifact_version=None,
-                 artifact_description=None,
-                 internal_version=None,
-                 created_date=None,
-                 artifact_name=None,
-                 published='N',
-                 updated_by=None,
-                 tags=None):
+    def __init__(self,  # pylint: disable=too-many-arguments
+                 blueprint_model_id: str,
+                 artifact_uuid: str = None,
+                 artifact_type: str = None,
+                 artifact_version: str = None,
+                 artifact_description: str = None,
+                 internal_version: str = None,
+                 created_date: str = None,
+                 artifact_name: str = None,
+                 published: str = 'N',
+                 updated_by: str = None,
+                 tags: str = None):
+        """Blueprint Model initialization.
 
+        Args:
+            blueprint_model_id (str): Blueprint model identifier
+            artifact_uuid (str): Blueprint model uuid
+            artifact_type (str): Blueprint artifact type
+            artifact_version (str): Blueprint model version
+            artifact_description (str): Blueprint model description
+            internal_version (str): Blueprint model internal version
+            created_date (str): Blueprint model created date
+            artifact_name (str): Blueprint model name
+            published (str): Blueprint model publish status - 'N' or 'Y'
+            updated_by (str): Blueprint model author
+            tags (str): Blueprint model tags
+
+        """
         super().__init__()
         self.blueprint_model_id = blueprint_model_id
         self.artifact_uuid = artifact_uuid
@@ -40,16 +56,16 @@ class BlueprintModel(CdsElement):  # pylint: disable=too-many-instance-attribute
     def __repr__(self) -> str:
         """Representation of object.
 
-               Returns:
-                   str: Object's string representation
+        Returns:
+           str: Object's string representation
 
-               """
+        """
         return (f"BlueprintModel(artifact_name='{self.artifact_name}', "
                 f"blueprint_model_id='{self.blueprint_model_id}')")
 
     @classmethod
     def get_by_id(cls, blueprint_model_id: str) -> "BlueprintModel":
-        """Retrieve blueprint model with provided ID
+        """Retrieve blueprint model with provided ID.
 
         Args: blueprint_model_id (str):
 
@@ -58,16 +74,16 @@ class BlueprintModel(CdsElement):  # pylint: disable=too-many-instance-attribute
 
         Raises:
             ValueError: Blueprint model with provided ID doesn't exist
-        """
 
+        """
         try:
             blueprint_model = cls.send_message_json(
-                    "GET",
-                    "Retrieve all blueprints",
-                    f"{cls._url}/api/v1/blueprint-model/{blueprint_model_id}",
-                    headers={},
-                    auth=cls.auth,
-                    exception=ValueError)
+                "GET",
+                "Retrieve all blueprints",
+                f"{cls._url}/api/v1/blueprint-model/{blueprint_model_id}",
+                headers={},
+                auth=cls.auth,
+                exception=ValueError)
 
             return BlueprintModel(
                 blueprint_model_id=blueprint_model["blueprintModel"]['id'],
@@ -88,7 +104,7 @@ class BlueprintModel(CdsElement):  # pylint: disable=too-many-instance-attribute
     @classmethod
     def get_by_name_and_version(cls, blueprint_name: str,
                                 blueprint_version: str) -> "BlueprintModel":
-        """Retrieve blueprint model with provided name and version
+        """Retrieve blueprint model with provided name and version.
 
         Args:
             blueprint_name (str): Blueprint model name
@@ -99,17 +115,17 @@ class BlueprintModel(CdsElement):  # pylint: disable=too-many-instance-attribute
 
         Raises:
             ValueError: Blueprint model with provided name and version doesn't exist
-        """
 
+        """
         try:
             blueprint_model = cls.send_message_json(
-                    "GET",
-                    "Retrieve all blueprints",
-                    f"{cls._url}/api/v1/blueprint-model/by-name/{blueprint_name}"
-                    f"/version/{blueprint_version}",
-                    headers={},
-                    auth=cls.auth,
-                    exception=ValueError)
+                "GET",
+                "Retrieve all blueprints",
+                f"{cls._url}/api/v1/blueprint-model/by-name/{blueprint_name}"
+                f"/version/{blueprint_version}",
+                headers={},
+                auth=cls.auth,
+                exception=ValueError)
 
             return BlueprintModel(
                 blueprint_model_id=blueprint_model["blueprintModel"]['id'],
@@ -130,12 +146,12 @@ class BlueprintModel(CdsElement):  # pylint: disable=too-many-instance-attribute
 
     @classmethod
     def get_all(cls) -> Iterator["BlueprintModel"]:
-        """Get all blueprint models
+        """Get all blueprint models.
 
         Yields:
             BlueprintModel: BlueprintModel object.
-        """
 
+        """
         for blueprint_model in cls.send_message_json(
                 "GET",
                 "Retrieve all blueprints",
@@ -158,12 +174,12 @@ class BlueprintModel(CdsElement):  # pylint: disable=too-many-instance-attribute
             )
 
     def get_blueprint(self) -> Blueprint:
-        """Get Blueprint object for selected blueprint model
+        """Get Blueprint object for selected blueprint model.
 
         Returns:
             Blueprint: Blueprint object
-        """
 
+        """
         cba_package = self.send_message(
             "GET",
             "Retrieve selected blueprints",
@@ -175,12 +191,11 @@ class BlueprintModel(CdsElement):  # pylint: disable=too-many-instance-attribute
         return Blueprint(cba_file_bytes=cba_package.content)
 
     def save(self, dst_file_path: str):
-        """Save blueprint model to file
+        """Save blueprint model to file.
 
         Args:
             dst_file_path (str): Path of file where blueprint is going to be saved
         """
-
         cba_package = self.send_message(
             "GET",
             "Retrieve selected blueprints",
@@ -192,11 +207,9 @@ class BlueprintModel(CdsElement):  # pylint: disable=too-many-instance-attribute
         with open(dst_file_path, "wb") as content:
             for chunk in cba_package.iter_content(chunk_size=128):
                 content.write(chunk)
-        return
 
     def delete(self):
-        """Delete blueprint model"""
-
+        """Delete blueprint model."""
         self.send_message(
             "DELETE",
             "Delete blueprint",
@@ -204,4 +217,3 @@ class BlueprintModel(CdsElement):  # pylint: disable=too-many-instance-attribute
             headers={},
             auth=self.auth,
             exception=ValueError)
-        return
