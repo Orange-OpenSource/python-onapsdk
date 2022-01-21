24a33f33d2e5dcf6b7338ca566182111bae4f742
Piotr Stanior <piotr.stanior@t-mobile.pl>
2022-01-21 14:13:10 +0100

    Add test for get tenant by name in cloud region

--- tests/test_aai_cloud_region.py
+++ tests/test_aai_cloud_region.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 from unittest import mock
 
 
-from onapsdk.aai.cloud_infrastructure.cloud_region import CloudRegion
+from onapsdk.aai.cloud_infrastructure.cloud_region import CloudRegion, Tenant
 from onapsdk.exceptions import ResourceNotFound
 
 
@@ -34,3 +34,11 @@ def test_cloud_region_complex_property(mock_complex_get, mock_relationships):
 
     mock_relationships.side_effect = ResourceNotFound
     assert cr.complex is None
+
+@mock.patch("onapsdk.aai.cloud_infrastructure.cloud_region.CloudRegion.tenants", new_callable=mock.PropertyMock)
+def test_cloud_region_get_tenant_by_name(mock_tenants):
+    cr = CloudRegion("test_cloud_owner", "test_cloud_region_id", False, False)
+    mock_tenants.return_value = iter([
+        Tenant(cloud_region="test_cloud_region_id",tenant_id="test-tenant",tenant_name="test-tenant")
+    ])
+    assert cr.get_tenant_by_name(tenant_name="test-tenant") is not None
