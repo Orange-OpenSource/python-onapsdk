83c8791b464d1f036153b68de111e12d320c4e4c
Michał Jagiełło <michal.jagiello@t-mobile.pl>
2022-09-02 05:44:33 +0000

    Resolve "Keep HTTP request status code in APIError class"

---
+++ tests/test_exceptions.py
@@ -0,0 +1,11 @@
+
+from onapsdk.exceptions import APIError
+
+
+def test_api_error_response_status_code():
+    err = APIError()
+    assert err.response_status_code == 0
+    err.response_status_code = 404
+    assert err.response_status_code == 404
+    err = APIError(response_status_code=404)
+    assert err.response_status_code == 404
